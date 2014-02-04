5007b5664c8112667407df7b41fa4bf2017bfa78
Alexander K <xpyro@ya.ru>
2014-02-04 12:23:25 +0000

    Blank redmine plugin

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :my do
+  name 'My plugin'
+  author 'Alexander K'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
